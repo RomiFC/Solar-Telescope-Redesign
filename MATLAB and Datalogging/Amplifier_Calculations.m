695afb771e0612858c2ca83872d0c8e2245a34ec
Remy <remyvnguyen@gmail.com>
2023-06-27 16:34:20 -0500

    Schematic Revision D Draft, Datasheets added

--- MATLAB and Datalogging/Amplifier_Calculations.m
+++ MATLAB and Datalogging/Amplifier_Calculations.m
@@ -5,6 +5,14 @@
 ZX60A = sparameters('ZX60-83LN-S+ (A), 30dB Output Attenuator.s2p');
 ALS = sparameters('ALS-04-0149 30dB Output Attenuator.s2p');
 S21_ZX = mag2db( abs( rfparam(ZX60A, 2, 1) )) + 30;     % 30dB Normalized
-Freq = rfparam(s2abcd(ZX60A, 50), 'D');
+%Freq = rfparam(s2abcd(ZX60A, 50), 'D');
 
 % Graphing S-Parameters
+figure(1)
+tiledlayout('flow')
+nexttile
+rfplot(ZX60A, 2, 1)
+grid on
+title('ZX60-83LN-S+')
+xlabel('Frequency (MHz)')
+ylabel('S21 (dB)')
