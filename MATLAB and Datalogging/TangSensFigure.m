b353f6d8a7234f1226fe27c92126243bf682189b
Remy <remyvnguyen@gmail.com>
2023-07-28 15:49:00 -0600

    Added new tansens figure

---
+++ MATLAB and Datalogging/TangSensFigure.m
@@ -0,0 +1,15 @@
+clear
+clc
+% Generates an oscilloscope plot of a tangential sensitivity measurement
+
+syms x
+f1(x) = piecewise(-0.5 <= x < 0, 0.4, 0 < x <= 0.25, 1, 0.25 < x <= 0.5, 0.4);
+
+x1 = [-0.5: 0.001: 0.5];
+u1 = 0.8*rand(size(x1)) - 0.5;
+y = double(f1(x1) + u1)*1000;   %Scales to mV
+csv = [x1; y]';
+writematrix(csv, 'exampleTS.txt', 'Delimiter', 'tab');
+
+plot(x1, y)
+grid on
