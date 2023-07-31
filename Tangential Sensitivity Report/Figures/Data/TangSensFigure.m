6d0c9a126f22f029395ed53db6233fe726026d64
Remy <remyvnguyen@gmail.com>
2023-07-31 14:12:27 -0600

    Started tansens report

--- Tangential Sensitivity Report/Figures/Data/TangSensFigure.m
+++ Tangential Sensitivity Report/Figures/Data/TangSensFigure.m
@@ -7,7 +7,7 @@ f1(x) = piecewise(-0.5 <= x < 0, 0.4, 0 < x <= 0.25, 1, 0.25 < x <= 0.5, 0.4);
 
 x1 = [-0.5: 0.001: 0.5];
 u1 = 0.8*rand(size(x1)) - 0.5;
-y = double(f1(x1) + u1)*1000;   %Scales to mV
+y = double(f1(x1) + u1);
 csv = [x1; y]';
 writematrix(csv, 'exampleTS.txt', 'Delimiter', 'tab');
 
