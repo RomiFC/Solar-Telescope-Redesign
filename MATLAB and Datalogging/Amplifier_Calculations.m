1ad01abe2b0443c5fd4c5b3d3fff12ecd0b7ecc2
Remy <remyvnguyen@gmail.com>
2023-08-02 16:23:02 -0600

    Added filter s-parameters

--- MATLAB and Datalogging/Amplifier_Calculations.m
+++ MATLAB and Datalogging/Amplifier_Calculations.m
@@ -4,6 +4,7 @@
 % Importing data
 ZX60A = sparameters('ZX60-83LN-S+ (A), 30dB Output Attenuator.s2p');
 ALS = sparameters('ALS-04-0149 30dB Output Attenuator.s2p');
+filter = sparameters('Unmarked Bandpass Filter.s2p');
 S21_ZX = mag2db( abs( rfparam(ZX60A, 2, 1) )) + 30;     % 30dB Normalized
 %Freq = rfparam(s2abcd(ZX60A, 50), 'D');
 
@@ -14,5 +15,13 @@ nexttile
 rfplot(ZX60A, 2, 1)
 grid on
 title('ZX60-83LN-S+')
+xlabel('Frequency (MHz)')
+ylabel('S21 (dB)')
+
+% Filter
+nexttile
+rfplot(filter, 2, 1)
+grid on
+title('Unmarked Bandpass Filter')
 xlabel('Frequency (MHz)')
 ylabel('S21 (dB)')
