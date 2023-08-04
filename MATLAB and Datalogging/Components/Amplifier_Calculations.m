830f1527e4540815e054026d641295ef9b9665b8
Remy <remyvnguyen@gmail.com>
2023-08-04 16:31:59 -0600

    Component S-parameters added to report for reference (temporary)

--- MATLAB and Datalogging/Components/Amplifier_Calculations.m
+++ MATLAB and Datalogging/Components/Amplifier_Calculations.m
@@ -7,6 +7,7 @@ clc
 ZX60A = sparameters('ZX60-83LN-S+ (A), 30dB Output Attenuator.s2p');
 ALS = sparameters('ALS-04-0149 30dB Output Attenuator.s2p');
 filter = sparameters('Unmarked Bandpass Filter.s2p');
+S21_filter = mag2db( abs( rfparam(filter, 2, 1) ));
 S21_ZX60 = mag2db( abs( rfparam(ZX60A, 2, 1) )) + 30;     % 30dB Normalized
 S21_ALS = mag2db( abs( rfparam(ALS, 2, 1) )) + 30;        % 30dB Normalized
 freq = ZX60A.Frequencies;
@@ -20,7 +21,7 @@ hold on
 rfplot(ALS, 2, 1)
 grid on
 title('ZX60-83LN-S+ (Blue) and ALS-04-0149 (Red)')
-xlabel('Frequency (MHz)')
+xlabel('Frequency (GHz)')
 ylabel('S21 (dB)')
 
 % Filter
@@ -28,7 +29,7 @@ nexttile
 rfplot(filter, 2, 1)
 grid on
 title('Unmarked Bandpass Filter')
-xlabel('Frequency (MHz)')
+xlabel('Frequency (GHz)')
 ylabel('S21 (dB)')
 
 % Mean LNA gain in the passband
@@ -38,3 +39,6 @@ pbGainZX60 = S21_ZX60(L, :);
 pbGainALS = S21_ALS(L, :);
 disp(mean(pbGainZX60))
 disp(mean(pbGainALS))
+
+% Output csv
+writematrix([freq/10^6, S21_ZX60, S21_ALS, S21_filter], 'Amplifiers and Filter Sparam.txt', 'Delimiter', 'tab');
