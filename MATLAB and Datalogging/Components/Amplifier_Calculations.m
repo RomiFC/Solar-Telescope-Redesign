34cacd6b55a90f03a6a2f0660cc7d6235b8c8688
Remy <remyvnguyen@gmail.com>
2023-08-04 13:51:22 -0600

    New spectrum analyzer traces added

--- MATLAB and Datalogging/Components/Amplifier_Calculations.m
+++ MATLAB and Datalogging/Components/Amplifier_Calculations.m
@@ -1,3 +1,5 @@
+clear
+clc
 % Amplifier gain calculations for VLA Solar Telescope
 % S parameters measured with 30dB output attenuation
 
@@ -5,16 +7,19 @@
 ZX60A = sparameters('ZX60-83LN-S+ (A), 30dB Output Attenuator.s2p');
 ALS = sparameters('ALS-04-0149 30dB Output Attenuator.s2p');
 filter = sparameters('Unmarked Bandpass Filter.s2p');
-S21_ZX = mag2db( abs( rfparam(ZX60A, 2, 1) )) + 30;     % 30dB Normalized
-%Freq = rfparam(s2abcd(ZX60A, 50), 'D');
+S21_ZX60 = mag2db( abs( rfparam(ZX60A, 2, 1) )) + 30;     % 30dB Normalized
+S21_ALS = mag2db( abs( rfparam(ALS, 2, 1) )) + 30;        % 30dB Normalized
+freq = ZX60A.Frequencies;
 
 % Graphing S-Parameters
 figure(1)
 tiledlayout('flow')
 nexttile
 rfplot(ZX60A, 2, 1)
+hold on
+rfplot(ALS, 2, 1)
 grid on
-title('ZX60-83LN-S+')
+title('ZX60-83LN-S+ (Blue) and ALS-04-0149 (Red)')
 xlabel('Frequency (MHz)')
 ylabel('S21 (dB)')
 
@@ -25,3 +30,11 @@ grid on
 title('Unmarked Bandpass Filter')
 xlabel('Frequency (MHz)')
 ylabel('S21 (dB)')
+
+% Mean LNA gain in the passband
+L = freq > 7800*10^6 & freq < 8900*10^6;
+pbFreq = freq(L, :);
+pbGainZX60 = S21_ZX60(L, :);
+pbGainALS = S21_ALS(L, :);
+disp(mean(pbGainZX60))
+disp(mean(pbGainALS))
